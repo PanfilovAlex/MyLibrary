6805da25380876d73b5e277d82b0ddb6bf74495d
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-10-21 14:55:42 +0300

    Add controllets and models

---
+++ WebApiMyLib/WebApiMyLib/Models/Autor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMyLib.Models
+{
+    public class Autor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WebApiMyLib/WebApiMyLib/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMyLib.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public List<string> Category { get; set; }
+        public Autor Autor { get; set; }
+    }
+}

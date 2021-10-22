c705ba642347a65dab77fe9eedf0de2d46d61f47
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-10-22 19:07:59 +0300

    Создал класс  BookDBContext для базы данных

--- WebApiMyLib/WebApiMyLib/Models/BookDbContext.cs
+++ WebApiMyLib/WebApiMyLib/Models/BookDbContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace WebApiMyLib.Models
 {
-    public class BookDbContext
+    public class BookDbContext:DbContext
     {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
     }
 }

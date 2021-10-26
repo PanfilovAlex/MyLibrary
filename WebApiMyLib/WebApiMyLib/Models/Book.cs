7cceff323ad88ef82c9d507c01d1697aaae0f4bc
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-10-26 16:34:02 +0300

    Создал элементарную модель Book Создал локальный репозиторий MemoryRepository и итерфейс IRepository

---
+++ WebApiMyLib/WebApiMyLib/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMyLib.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Autor { get; set; }
+
+    }
+}

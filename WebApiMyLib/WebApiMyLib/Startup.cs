b7b8bae07f9ee3a4495c9b02cd461faf2b637641
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-10-27 14:49:07 +0300

    Добавил контроллер и визуализировал представление

--- WebApiMyLib/WebApiMyLib/Startup.cs
+++ WebApiMyLib/WebApiMyLib/Startup.cs
@@ -28,33 +28,23 @@ namespace WebApiMyLib
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiMyLib", Version = "v1" });
-            });
+            services.AddSingleton<IBookRepository, MemoryRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiMyLib v1"));
-            }
-
-            app.UseHttpsRedirection();
-
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    null,
+                    "{controller=Home}/{action=Index}");
             });
         }
     }

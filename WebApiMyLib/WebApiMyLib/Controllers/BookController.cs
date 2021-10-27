53da620d932a58e0de42ae9fddd819a8a2f698bb
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-10-27 15:32:09 +0300

    добавил контоллер для REST api

---
+++ WebApiMyLib/WebApiMyLib/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiMyLib.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace WebApiMyLib.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private IBookRepository _repository;
+        public BookController(IBookRepository bookRepository)
+        {
+            _repository = bookRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get() => _repository.Books;
+        [HttpGet("{id}")]
+        public Book Get(int id)
+        {
+            return _repository[id];
+        }
+
+        [HttpPost]
+        public Book Post([FromBody] Book book) => _repository.AddBook(new Book
+        {
+            Autor = book.Autor,
+            Title = book.Title,
+            Category = book.Category
+        });
+
+        [HttpPut]
+        public Book Put([FromBody] Book book) => _repository.UpdateBook(book);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<Book> patch)
+        {
+            Book book = Get(id);
+            if (book != null)
+            {
+                patch.ApplyTo(book);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => _repository.DeleteBook(id); 
+    }
+}

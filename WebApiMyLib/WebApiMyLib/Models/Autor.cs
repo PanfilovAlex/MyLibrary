4a9143f3855b057510abb8a725b8ba643aa69819
PanfilovAlex <64968108+PanfilovAlex@users.noreply.github.com>
2021-11-12 09:56:12 +0300

    Добавил класс "Autor"

---
+++ WebApiMyLib/WebApiMyLib/Models/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiMyLib.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+        public bool IsDeleted { get; set; }
+        public Autor()
+        {
+            Books = new List<Book>();
+        }
+        
+    }
+}
